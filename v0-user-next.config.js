b2fb8748d7513a108e46c110c16cfd5f540535a8
sefexsaiieh <sefex.saiieh@gmail.com>
2025-02-20 12:16:05 +0100

    3rd

--- v0-user-next.config.js
+++ v0-user-next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   env: {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  },
 }
 
 module.exports = nextConfig
